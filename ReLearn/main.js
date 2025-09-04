8a3a1d3c4bb905145caa76a080a1549d6d7feb55
Mohammed Ali Abd ElShaheed <engineer.mohammed.ali11@gmail.com>
2025-09-04 23:40:42 +0300

    after end 2 hours

--- ReLearn/main.js
+++ ReLearn/main.js
@@ -1,3 +1,38 @@
-window.alert("hello");
-document.write("hello");
-docuemnt.write("tata");
+// document.write("tata");
+// console.log(hello);
+/* console.log("Elzero\n\nWeb\n\nSchool");
+console.log(`Elzero Web School`)
+console.log(
+`Elzero
+Web
+School`
+) */
+
+////////////////////////////////////////////////////////
+
+/* let theTitle = "Elzero";
+let theDesc = "Elzero Web School";
+let theDate = "20/5";
+let card =
+`<div class ="card">
+    <div class ="container">
+        <h1>${theTitle}</h1>
+        <p>${theDesc}</p>
+        <span>${theDate}</span>
+    </div>
+</div>`;
+document.write("#".repeat(50));
+document.write(card.repeat(4));
+document.write("#".repeat(50)); */
+
+//////////////////////////////////////////////////
+
+/* console.log("elzero web %csch%cool", "color: blue;font-size: 50px;", "color: red;font-size: 50px;");
+
+let names = ["ah", "ali", "mo"]
+
+console.table(names) */
+
+///////////////////////////////////////////////////
+
+console.log(100..toString())
